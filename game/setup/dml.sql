bf91b31206f6409830ffa73dc73349dcbbf38696
Victor Rayan Ferreira <victor.rayan378@gmail.com>
2022-07-27 20:34:39 -0300

    Update dml.sql

--- game/setup/dml.sql
+++ game/setup/dml.sql
@@ -1,18 +1,18 @@
 INSERT INTO barco (id_barco, nome, ocupacao, grupo_ocupacao, capacidade_de_itens)
 VALUES 
-(default, 'Thousand Sunny', 'value3', 'value4', 400);
-(default, 'Miss Love Duck', 'value3', 'value4', 250);
-(default, 'Utan Sonar', 'value3', 'value4', 300);
-(default, 'Moby Dick', 'value3', 'value4', 350);
+(default, 'Thousand Sunny', 'value3', 'value4', 400),
+(default, 'Miss Love Duck', 'value3', 'value4', 250),
+(default, 'Utan Sonar', 'value3', 'value4', 300),
+(default, 'Moby Dick', 'value3', 'value4', 350),
 (default, 'Holandês Voador', 'value3', 'value4', 500);
 
 INSERT INTO poder_especial (nome, id_personagem_principal, descricao, dano, energia)
 VALUES 
-('Gomu gomu no Hammer', auto, 'cascudo com as duas mãos', 300, 400);
+('Gomu gomu no Hammer', auto, 'cascudo com as duas mãos', 300, 400),
 
-('Bounceman',auto,' cobre seus braços com Busoshoku Haki antes de morder seu antebraço. Semelhante à forma como ele ativa o Gear Third, ele sopra uma incrível quantidade de ar em seu corpo, mas desta vez ele infla sua estrutura muscular antes de distribuir o ar em todo o seu corpo, com ênfase na sua metade superior.',800,250);
-('Tankman', auto, 'Nesta forma, ele se assemelha a uma bola com proporções exageradas: enquanto seus braços e pernas são ligeiramente pequenos em relação ao corpo, seu torso com Busoshoku Haki-revestido torna-se muito maior e completamente redondo, embora ainda há músculos nos braços e no peitoral.', 900, 300);
-('Snakeman', auto, 'Em vez de vapor, essa forma produz uma substância gasosa que aparece como uma mistura de vapor e fogo e, como suas outras contrapartes, também parece ser colocada sobre seus ombros como um lenço, enquanto sob esta forma. Seus punhos também se flexionam diferentemente, com os primeiros nós dos dedos sendo estendidos, semelhante ao punho de leopardo.', 950, 350);
-('Kinniku Fusen', auto, 'dá um soco como um Gomu Gomu no Pistol, mas ele pode redirecionar seu soco à um número indeterminado de vezes, sem perder o impulso, mantendo a sua força tremenda.', 1000, 500)
+('Bounceman',auto,' cobre seus braços com Busoshoku Haki antes de morder seu antebraço. Semelhante à forma como ele ativa o Gear Third, ele sopra uma incrível quantidade de ar em seu corpo, mas desta vez ele infla sua estrutura muscular antes de distribuir o ar em todo o seu corpo, com ênfase na sua metade superior.',800,250),
+('Tankman', auto, 'Nesta forma, ele se assemelha a uma bola com proporções exageradas: enquanto seus braços e pernas são ligeiramente pequenos em relação ao corpo, seu torso com Busoshoku Haki-revestido torna-se muito maior e completamente redondo, embora ainda há músculos nos braços e no peitoral.', 900, 300),
+('Snakeman', auto, 'Em vez de vapor, essa forma produz uma substância gasosa que aparece como uma mistura de vapor e fogo e, como suas outras contrapartes, também parece ser colocada sobre seus ombros como um lenço, enquanto sob esta forma. Seus punhos também se flexionam diferentemente, com os primeiros nós dos dedos sendo estendidos, semelhante ao punho de leopardo.', 950, 350),
+('Kinniku Fusen', auto, 'dá um soco como um Gomu Gomu no Pistol, mas ele pode redirecionar seu soco à um número indeterminado de vezes, sem perder o impulso, mantendo a sua força tremenda.', 1000, 500);
 
 -- https://onepiece.fandom.com/pt/wiki/Navios
