5fa867749c19e1c8a32becb3fe82cde9210a85d6
Thalisson-Alves <thalisson2030@gmail.com>
2022-07-28 15:35:38 -0300

    add first missions

---
+++ game/setup/dml.sql
@@ -0,0 +1,47 @@
+INSERT INTO missao_principal (nome, descricao, qtd_experiencia, dificuldade, id_personagem)
+VALUES ('Começo da jornada', 'Luffy derrota Alvida e segue sua viagem junto de Coby pra formar uma tripulação', 10, 0, NULL);
+
+    INSERT INTO objetivo (id_missao, nome, descricao, tipo, id_item, id_inimigo, id_nao_hostil)
+    VALUES (1, '', 'Luffy derrota piratas do bando da Alvida', 'DerrotarInimigo', NULL, NULL, NULL);
+
+    INSERT INTO objetivo (id_missao, nome, descricao, tipo, id_item, id_inimigo, id_nao_hostil)
+    VALUES (1, '', 'Luffy da o golpe final e manda Alvida pelos ares', 'DerrotarInimigo', NULL, NULL, NULL);
+
+INSERT INTO missao_principal (nome, descricao, qtd_experiencia, dificuldade, id_personagem)
+VALUES ('O grande espadachim aparece', 'Luffy e Coby conhecem e libertam o Caçador de Piratas Roronoa Zoro, que foi preso pelo Capitão Morgan e seu filho Helmeppo', 10, 0, NULL);
+
+    INSERT INTO objetivo (id_missao, nome, descricao, tipo, id_item, id_inimigo, id_nao_hostil)
+    VALUES (2, '', 'Luffy derrota os marinheiros que estavam guardando as espadas de Roronoa Zoro', 'DerrotarInimigo', NULL, NULL, NULL);
+
+    INSERT INTO objetivo (id_missao, nome, descricao, tipo, id_item, id_inimigo, id_nao_hostil)
+    VALUES (2, '', 'Luffy pega as espadas de Roronoa Zoro, que tinham sido confiscadas pela marinha', 'PegarItem', NULL, NULL, NULL);
+
+    INSERT INTO objetivo (id_missao, nome, descricao, tipo, id_item, id_inimigo, id_nao_hostil)
+    VALUES (2, '', 'Luffy liberta Roronoa Zoro e devolve suas espadas', 'FalarComNPC', NULL, NULL, NULL);
+
+INSERT INTO missao_principal (nome, descricao, qtd_experiencia, dificuldade, id_personagem)
+VALUES ('Morgan VS Luffy', 'Luffy e o mais novo membro de sua tripulação, Roronoa Zoro, lutam contra o Capitão Morgan', 10, 0, NULL);
+
+    INSERT INTO objetivo (id_missao, nome, descricao, tipo, id_item, id_inimigo, id_nao_hostil)
+    VALUES (3, '', 'Luffy e Zoro derrotam os marinheiros que estavam protegendo o Capitão Morgan', 'DerrotarInimigo', NULL, NULL, NULL);
+
+    INSERT INTO objetivo (id_missao, nome, descricao, tipo, id_item, id_inimigo, id_nao_hostil)
+    VALUES (3, '', 'Luffy e Zoro vencem o Capitão Morgan', 'DerrotarInimigo', NULL, NULL, NULL);
+
+    INSERT INTO objetivo (id_missao, nome, descricao, tipo, id_item, id_inimigo, id_nao_hostil)
+    VALUES (3, '', 'Coby se dispede de Luffy para seguir sua ambição de se tornar um marinheiro.', 'FalarComNPC', NULL, NULL, NULL);
+
+INSERT INTO missao_principal (nome, descricao, qtd_experiencia, dificuldade, id_personagem)
+VALUES ('Palhaço-Pirata, Capitão Buggy', 'Luffy e Zoro se deparam o Capitão Buggy', 10, 0, NULL);
+
+    INSERT INTO objetivo (id_missao, nome, descricao, tipo, id_item, id_inimigo, id_nao_hostil)
+    VALUES (4, '', 'Luffy derrota piratas do Capitão Buggy', 'DerrotarInimigo', NULL, NULL, NULL);
+
+    INSERT INTO objetivo (id_missao, nome, descricao, tipo, id_item, id_inimigo, id_nao_hostil)
+    VALUES (4, '', 'Nami engana Luffy e o entrega para Buggy', 'DerrotarInimigo', NULL, NULL, NULL);
+
+INSERT INTO missao_principal (nome, descricao, qtd_experiencia, dificuldade, id_personagem)
+VALUES ('Mohji, o domador VS Luffy', 'Luffy consegue se libertar da jaula em que estava preso e luta contra Mohji e Richie', 10, 0, NULL);
+
+    INSERT INTO objetivo (id_missao, nome, descricao, tipo, id_item, id_inimigo, id_nao_hostil)
+    VALUES (4, '', 'Luffy enfrenta Richie', 'DerrotarInimigo', NULL, NULL, NULL);
