98f5aa88017af4257a9977a8d47c5eef27aec98d
Luan-Cavalcante <luanv.cavalcante@gmail.com>
2022-07-31 22:31:02 -0300

    adicionando consultas

--- game/setup/consultas.sql
+++ game/setup/consultas.sql
@@ -4,35 +4,35 @@ FROM poder_especial;
 
 SELECT nome, descricao 
 FROM poder_especial 
-WHERE dano > 900 AND energia >= 300 
+WHERE dano > 900 AND energia >= 300;
 
-SELECT * FROM poder_especial WHERE nome='Tankman'
+SELECT * FROM poder_especial WHERE nome='Tankman';
 
 -- Missao
 SELECT qtd_experiencia 
 FROM missao 
-WHERE id_missao = 1
+WHERE id_missao = 1;
 
 UPDATE personagem_principal 
 SET experiencia = experiencia + 10 
-WHERE id_personagem = 1
+WHERE id_personagem = 1;
 
 -- Inventario
 
 SELECT inventario.id_personagem, item.nome,item.descricao, item.preco, item.qtd_energia, item.qtd_vida,item.qtd_dano, personagem_principal.nome
 from inventario 
 INNER JOIN item on inventario.id_item = item.id_item
-JOIN personagem_principal on inventario.id_personagem = personagem_principal.id_personagem
+JOIN personagem_principal on inventario.id_personagem = personagem_principal.id_personagem;
 
 -- Barco
 
 SELECT id_barco, nome, ocupacao, grupo_ocupacao, capacidade_de_itens
 FROM barco;
 
-SELECT * FROM barco WHERE nome='Moby Dick'
+SELECT * FROM barco WHERE nome='Moby Dick';
 
 -- Item
-SELECT * FROM item WHERE is_equipavel= 'TRUE'
+SELECT * FROM item WHERE is_equipavel= 'TRUE';
 SELECT * FROM item where is_equipavel = 'FALSE';
 
 SELECT nome, descricao, preco, qtd_energia, qtd_vida FROM item where nome = 'bergamota';
@@ -48,3 +48,26 @@ SELECT nome, descricao, preco, qtd_energia, qtd_vida, qtd_dano FROM item where n
 SELECT nome, ocupacao, grupo_ocupacao, is_vendedor
 FROM personagem_nao_hostil 
 WHERE id_personagem=%d
+
+-- Personagem principal
+
+SELECT * from personagem_principal;
+
+
+-- inimigo 
+
+select * from inimigo;
+
+select * from inimigo where vida > 100;
+
+select * from inimigo where ocupacao = 'Marinheiro';
+
+SELECT * FROM inimigo where ocupacao = 'Pirata';
+
+-- Ilha
+
+SELECT * FROM ilha;
+
+-- Região 
+
+
