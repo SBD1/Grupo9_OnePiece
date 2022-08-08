34c8e2ee9725d523959f53c4a86a814e0c522b43
Luan-Cavalcante <luanv.cavalcante@gmail.com>
2022-08-08 10:53:47 -0300

    reposicionando regiao

--- modulo_2/SQL/dml.sql
+++ modulo_2/SQL/dml.sql
@@ -1,4 +1,3 @@
-BEGIN;
 
 INSERT INTO item (id_item,nome, descricao, preco, qtd_energia, qtd_vida, qtd_dano, is_equipavel)
 VALUES
@@ -9,18 +8,48 @@ VALUES
 (5,'laminada', 'aumenta o dano do personagem quando equipado', 15, 60, 10, 15, TRUE),
 (6,'rapidez', 'aumenta o dano do personagem quando equipado', 8, 90, 5, 12, TRUE);
 
-INSERT INTO personagem_principal (id_personagem, nome,ocupacao,grupo_ocupacao,berries,energia,fraqueza,experiencia,vida,capacidade_de_itens) VALUES 
-(1,'Monkey D. Luffy','Pirata','Piratas do Chapéu de Palha',150,100,'Kairoseki',1,120,10),
-(2,'Roronoa Zoro','Pirata','Piratas do Chapéu de Palha',100,100,'Cortar Ferro',1,100,10),
-(3,'Sanji','Pirata','Piratas do Chapéu de Palha',1000,100,'',1,100,15),
-(4,'Nami','Pirata','Piratas do Chapéu de Palha',1000000,75,'Tritões',1,100,8);
+INSERT INTO ilha (id_ilha,nome,descricao) VALUES 
+(1,'Ilha do Capitão Morgan','Ilha com uma base da Marinha, comandada pelo Capitão Morgan. Um marinheiro temido tanto pela população quanto pela marinha.Zoro aparece capturado nessa ilha'),
+(2,'Orange Town','Ilha tomada pelo Buggy, cujo prefeito se chama Boodle'),
+(3,'Vila Syrup','Vila da ilhas Geecko, onde Usopp, um grande Guerreiro do mar com mais de 8 mil seguidores, vive'),
+(4,'Baratie','Restaurante Marítmo comandado pelo chefe Zeff. Zeff tem um filho chamado Sanji, um cozinheiro romântico que ama mulheres.'),
+(5,'Ilhas Conami','Arquipélogo de Ilhas onde Nami nasceu. Era um arquipélogo quieto, até Arlong e seus piratas chegarem.');
 
-INSERT INTO barco (id_barco,nome, ocupacao, grupo_ocupacao, capacidade_de_itens) VALUES 
-(1,'Thousand Sunny', 'Pirata', 'Piratas do Chapéu de Palha', 400),
-(2,'Miss Love Duck', 'Pirata', 'Piratas do Bon Chan', 250),
-(3,'Moby Dick', 'Pirata', 'Piratas do Barba Branca', 1000),
-(4,'Going Merry', 'Pirata', 'Piratas do Chapéu de Palha', 200),
-(5,'Oro Jackson', 'Pirata', 'Piratas do Roger', 1000);
+insert into regiao (descricao,tipo,norte,sul,leste,oeste,id_ilha)
+values 
+-- Ilha Capitão Morgan
+('Porto onde você pode viajar para outras cidades','Porto',2,NULL,NULL,4,1), -- 1
+('Feira onde você pode comprar itens','Cidade',3,NULL,1,NULL,1), -- 2
+('Taverna onde você encontra variedades da cachaça, mas o Luffy só toma leite','Cidade',NULL,3,2,NULL,1), -- 3
+('Base da Marinha, parece que Zoro se encontra lá, Converse com Rika.','Cidade',NULL,1,NULL,3,1),  -- 4
+--Ilha Buggy
+('Porto de Orange Town','Porto',6,NULL,7,NULL,2), -- 5
+('Loja Cachorro Xuxu','Cidade',NULL,7,5,NULL,2), -- 6
+('Base Buggy','Cidade',NULL,8,NULL,6,2), -- 7
+('Casa do Prefeito','Cidade',7,NULL,NULL,5,2),-- 8
+-- Ilha Usopp
+('Porto saída Sul','Porto',10,NULL,NULL,NULL,3), -- 9
+('Centro da cidade','Cidade',NULL,9,11,NULL,3), -- 10
+('Casa da Kaya','Cidade',12,NULL,10,NULL,3), -- 11
+('Porto da Saída Norte','Porto',NULL,11,NULL,NULL,3), -- 12
+-- Baratie
+('Porto','Porto',14,NULL,NULL,NULL,4), -- 13
+('Restaurante','Porto',NULL,11,NULL,NULL,3), -- 14
+('Deck','Porto',NULL,11,NULL,NULL,3), -- 15
+('Barco','Porto',NULL,11,NULL,NULL,3); -- 15
+
+INSERT INTO personagem_principal (id_personagem, nome,id_regiao,ocupacao,grupo_ocupacao,berries,energia,fraqueza,experiencia,vida,capacidade_de_itens) VALUES 
+(1,'Monkey D. Luffy',1,'Pirata','Piratas do Chapéu de Palha',150,100,'Kairoseki',1,120,10),
+(2,'Roronoa Zoro',2,'Pirata','Piratas do Chapéu de Palha',100,100,'Cortar Ferro',1,100,10),
+(3,'Sanji','Pirata',3,'Piratas do Chapéu de Palha',1000,100,'',1,100,15),
+(4,'Nami','Pirata',1,'Piratas do Chapéu de Palha',1000000,75,'Tritões',1,100,8);
+
+INSERT INTO barco (id_barco,id_regiao,nome, ocupacao, grupo_ocupacao, capacidade_de_itens) VALUES 
+(1,1,'Thousand Sunny', 'Pirata', 'Piratas do Chapéu de Palha', 400),
+(2,1,'Miss Love Duck', 'Pirata', 'Piratas do Bon Chan', 250),
+(3,1,'Moby Dick', 'Pirata', 'Piratas do Barba Branca', 1000),
+(4,1,'Going Merry', 'Pirata', 'Piratas do Chapéu de Palha', 200),
+(5,1,'Oro Jackson', 'Pirata', 'Piratas do Roger', 1000);
 
 -- poderia criar um tipo para poder especial e a fraqueza seria um tipo
 INSERT INTO poder_especial (nome,tipo_poder ,id_personagem, descricao, dano, energia) VALUES 
@@ -64,36 +93,30 @@ INSERT INTO poder_especial (nome,tipo_poder ,id_personagem, descricao, dano, ene
 ('','Ferro', 1, 'Em vez de vapor, essa forma produz uma substância gasosa que aparece como uma mistura de vapor e fogo e, como suas outras contrapartes, também parece ser colocada sobre seus ombros como um lenço, enquanto sob esta forma. Seus punhos também se flexionam diferentemente, com os primeiros nós dos dedos sendo estendidos, semelhante ao punho de leopardo.', 950, 350);
 
 
-INSERT INTO inimigo (nome,ocupacao,grupo_ocupacao,berries,energia,fraqueza,experiencia,vida) VALUES 
-('Marinheiro 1','Marinheiro','Marinha',150,100,'Akuma no mi',1,100),
-('Marinheiro 2','Marinheiro','Marinha',250,100,'Akuma no mi',2,100),
-('Marinheiro 3','Marinheiro','Marinha',500,100,'Raki',3,100),
-('Marinheiro 4','Marinheiro','Marinha',1000,100,'Raki do rei',4,100),
-('Akainu','Marinheiro','Almirante da Marinha',1000000,10000,'Kairoseki',10,10000),
-('Alkiji','Marinheiro','Almirante da Marinha',900000,10000,'Kairoseki',10,10000),
-('Kizaru','Marinheiro','Almirante da Marinha',1200000,10000,'Kairoseki',10,10000),
-('Alvida','Pirata','Piratas da Alvida',1000,100,'Akuma no mi',1,100),
-('Morgan','Marinheiro','Capitão da Marinha',3000,150,'Akuma no mi',1,100),
-('Buggy','Pirata','Piratas do Buggy',1000,200,'Akuma no mi',2,150),
-('Arlong','Pirata','Piratas do Arlong',50000,250,'Akuma no mi',3,250);
+INSERT INTO inimigo (id_regiao,nome,ocupacao,grupo_ocupacao,berries,energia,fraqueza,experiencia,vida) VALUES 
+(1,'Marinheiro 1','Marinheiro','Marinha',150,100,'Akuma no mi',1,100),
+(1,'Marinheiro 2','Marinheiro','Marinha',250,100,'Akuma no mi',2,100),
+(1,'Marinheiro 3','Marinheiro','Marinha',500,100,'Raki',3,100),
+(1,'Marinheiro 4','Marinheiro','Marinha',1000,100,'Raki do rei',4,100),
+(1,'Akainu','Marinheiro','Almirante da Marinha',1000000,10000,'Kairoseki',10,10000),
+(1,'Alkiji','Marinheiro','Almirante da Marinha',900000,10000,'Kairoseki',10,10000),
+(1,'Kizaru','Marinheiro','Almirante da Marinha',1200000,10000,'Kairoseki',10,10000),
+(1,'Alvida','Pirata','Piratas da Alvida',1000,100,'Akuma no mi',1,100),
+(1,'Morgan','Marinheiro','Capitão da Marinha',3000,150,'Akuma no mi',1,100),
+(1,'Buggy','Pirata','Piratas do Buggy',1000,200,'Akuma no mi',2,150),
+(1,'Arlong','Pirata','Piratas do Arlong',50000,250,'Akuma no mi',3,250);
 
-INSERT INTO ilha (id_ilha,nome,descricao) VALUES 
-(1,'Ilha do Capitão Morgan','Ilha com uma base da Marinha, comandada pelo Capitão Morgan. Um marinheiro temido tanto pela população quanto pela marinha.Zoro aparece capturado nessa ilha'),
-(2,'Orange Town','Ilha tomada pelo Buggy, cujo prefeito se chama Boodle'),
-(3,'Vila Syrup','Vila da ilhas Geecko, onde Usopp, um grande Guerreiro do mar com mais de 8 mil seguidores, vive'),
-(4,'Baratie','Restaurante Marítmo comandado pelo chefe Zeff. Zeff tem um filho chamado Sanji, um cozinheiro romântico que ama mulheres.'),
-(5,'Ilhas Conami','Arquipélogo de Ilhas onde Nami nasceu. Era um arquipélogo quieto, até Arlong e seus piratas chegarem.');
 
-INSERT INTO personagem_nao_hostil (id_personagem, nome, ocupacao, grupo_ocupacao, is_vendedor, is_personagem_historia)
+INSERT INTO personagem_nao_hostil (id_personagem, id_regiao,nome, ocupacao, grupo_ocupacao, is_vendedor, is_personagem_historia)
 VALUES
-(1,'Coby', 'Cidadao', 'Nenhum', false, true),
-(2,'vendedor', 'Cidadao', 'Comercializar itens basicos', true, false),
-(3,'Mãe da Rika', 'Cidadao', 'Dona de casa', false, true),
-(4,'Rika', 'Cidadao', 'Brincar', false, true),
-(5,'Marinheiro', 'Marinheiro', 'Patrulhar', false, false),
-(6,'Prefeito', 'Cidadao', 'Gerir um ilha', false, false),
-(7,'Comerciante', 'Cidadao', 'Vender e comprar itens de pessoas', true, false),
-(8,'Chuchu', 'Cidadao', 'Ser um cachorro', false, true);
+(1,1,'Coby', 'Cidadao', 'Nenhum', false, true),
+(2,1,'vendedor', 'Cidadao', 'Comercializar itens basicos', true, false),
+(3,1,'Mãe da Rika', 'Cidadao', 'Dona de casa', false, true),
+(4,1,'Rika', 'Cidadao', 'Brincar', false, true),
+(5,1,'Marinheiro', 'Marinheiro', 'Patrulhar', false, false),
+(6,1,'Prefeito', 'Cidadao', 'Gerir um ilha', false, false),
+(7,1,'Comerciante', 'Cidadao', 'Vender e comprar itens de pessoas', true, false),
+(8,1,'Chuchu', 'Cidadao', 'Ser um cachorro', false, true);
 
 INSERT INTO inventario (id_personagem, id_item, qtd_item)
 VALUES 
@@ -124,32 +147,7 @@ INSERT INTO objetivo (id_missao, nome, descricao, tipo, id_item, id_inimigo, id_
 (4, '', 'Luffy derrota piratas do Capitão Buggy', 'DerrotarInimigo', NULL, NULL, NULL),
 (4, '', 'Nami engana Luffy e o entrega para Buggy', 'DerrotarInimigo', NULL, NULL, NULL),
 (4, '', 'Luffy enfrenta Richie', 'DerrotarInimigo', NULL, NULL, NULL);
-    
-
-insert into regiao (descricao,tipo,norte,sul,leste,oeste,id_ilha)
-values 
--- Ilha Capitão Morgan
-('Porto onde você pode viajar para outras cidades','Porto',2,NULL,NULL,4,1), -- 1
-('Feira onde você pode comprar itens','Cidade',3,NULL,1,NULL,1), -- 2
-('Taverna onde você encontra variedades da cachaça, mas o Luffy só toma leite','Cidade',NULL,3,2,NULL,1), -- 3
-('Base da Marinha, parece que Zoro se encontra lá, Converse com Rika.','Cidade',NULL,1,NULL,3,1),  -- 4
---Ilha Buggy
-('Porto de Orange Town','Porto',6,NULL,7,NULL,2), -- 5
-('Loja Cachorro Xuxu','Cidade',NULL,7,5,NULL,2), -- 6
-('Base Buggy','Cidade',NULL,8,NULL,6,2), -- 7
-('Casa do Prefeito','Cidade',7,NULL,NULL,5,2),-- 8
--- Ilha Usopp
-('Porto saída Sul','Porto',10,NULL,NULL,NULL,3), -- 9
-('Centro da cidade','Cidade',NULL,9,11,NULL,3), -- 10
-('Casa da Kaya','Cidade',12,NULL,10,NULL,3), -- 11
-('Porto da Saída Norte','Porto',NULL,11,NULL,NULL,3); -- 12
--- Baratie
-('Porto','Porto',14,NULL,NULL,NULL,4); -- 13
-('Restaurante','Porto',NULL,11,NULL,NULL,3); -- 14
-('Deck','Porto',NULL,11,NULL,NULL,3); -- 15
-('Barco','Porto',NULL,11,NULL,NULL,3); -- 15
 
 
 
 
-commit;
