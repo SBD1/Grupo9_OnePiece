c3bf292934aee0f633fe1f94030d517b3f44851e
Thalisson-Alves <thalisson2030@gmail.com>
2022-08-15 09:19:55 -0300

    remove database::dao

--- game/database.py
+++ game/database.py
@@ -1,7 +1,6 @@
 import os
 
 import psycopg2
-from psycopg2.extensions import cursor
 
 
 class Database:
@@ -16,9 +15,6 @@ class Database:
         )
         self.cursor = self.connection.cursor()
 
-        if not Database.TABLE_FIELDS:
-            self.__load_tables()
-
     def __enter__(self):
         self.connection.__enter__()
         return self
@@ -26,51 +22,8 @@ class Database:
     def __exit__(self, *args, **kwargs):
         self.connection.__exit__(*args, **kwargs)
 
-    def __getattribute__(self, name: str) -> 'Dao':
-        if name in Database.TABLE_FIELDS:
-            return Dao(name, Database.TABLE_FIELDS[name], self.cursor)
-        return super().__getattribute__(name)
-
-    def __load_tables(self) -> None:
-        self.cursor.execute('SELECT table_name FROM information_schema.tables WHERE table_schema=%s AND table_type=%s', [
-                            'public', 'BASE TABLE'])
-        table_names = [names[0] for names in self.cursor.fetchall()]
-        for table_name in table_names:
-            self.cursor.execute(
-                'SELECT column_name FROM information_schema.columns WHERE table_name=%s', [table_name])
-            Database.TABLE_FIELDS[table_name] = [columns[0]
-                                                 for columns in self.cursor.fetchall()]
-
     def __del__(self):
         self.close()
 
     def close(self) -> None:
         self.connection.close()
-
-
-class Dao:
-    SELECT_QUERY = 'SELECT {fields} FROM {table} {filters}'
-
-    def __init__(self, table_name: str, fields: list, cursor: cursor) -> None:
-        self.table_name = table_name
-        self.fields = fields
-        self.cursor = cursor
-
-    def get(self, filters: dict = None, fields: list = None):
-        fields = fields or self.fields
-        self.cursor.execute(Dao.SELECT_QUERY.format(
-            fields=','.join(fields),
-            table=self.table_name,
-            filters=self.generate_filters(filters)
-        ), [*filters.values()])
-        return [{k: v for k, v in zip(fields, result)}
-                for result in self.cursor.fetchall()]
-
-    def generate_filters(self, filters: dict) -> str:
-        if not filters:
-            return ''
-        result = ' AND '.join('{column} {operator} %s'.format(
-            column=column,
-            operator='='
-        ) for column in filters.keys())
-        return f'WHERE {result}'
