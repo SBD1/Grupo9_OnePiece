513cd71c3b520a0375351c0c6cca1f5eb840c8a6
Luan-Cavalcante <luanv.cavalcante@gmail.com>
2022-08-22 11:53:12 -0300

    adição pasta modulo 3

---
+++ modulo_3/triggger_stored_procedure.sql
@@ -0,0 +1,21 @@
+
+--Vitor
+    -- Cria jogador, Cria um personagem principal pro Jogador #cria instancia de Inimigo,principal,não-hostil 
+    -- objetivo cumprido pra liberar o próximo - tabela
+
+-- Bernardo
+    -- check missão cumprida pra liberar outra missão
+    -- dropa item 
+
+-- Thalisson
+    -- respawn e spawn dos inimigos em determinada região qnd tu entra lá.
+    -- compra de itens
+
+-- Nicolas 
+    -- Atualiza nível consequentemente atualiza poder especial
+    -- Inventário lotado, não pode receber item.
+
+-- Luan Vasco
+    -- compra um item tem que diminuir o dinheiro
+    -- Quando conclui missão, libera ilha
+        -- conclui objetivo, marca ilha liberada
