55a4b94a3d0a41a9a96bd797fc44dfb20cc8853c
Luan-Cavalcante <luanv.cavalcante@gmail.com>
2022-08-25 22:42:02 -0300

    arquivo de triggers

--- modulo_3/triggger_stored_procedure.sql
+++ modulo_3/triggger_stored_procedure.sql
@@ -31,7 +31,132 @@ CREATE TRIGGER spawn_inimigo_trigger
     -- Atualiza nível consequentemente atualiza poder especial
     -- Inventário lotado, não pode receber item.
 
--- Luan Vasco
+-- check_missao Vasco
     -- compra um item tem que diminuir o dinheiro
+
+-- antes do insert ================== tem que checar se está vazio o inventário
+-- tirar dinheiro do personagem principal && jogador
+-- preço do item
+-- como funciona a compra ? 
+--      vc fala com um npc e o npc te oferece itens do inventário dele
+--      se aceitar a compra, tem que tirar a grana do jogador e incrementar no npc 
+
+CREATE OR REPLACE procedure compra2(preco INTEGER, nome_jog VARCHAR(30),persona INTEGER)
+AS $compra2$
+declare 
+        berry INTEGER;
+BEGIN
+    
+    Select berries into berry from jogador 
+    WHERE nome_save = nome_jog and id_personagem = persona;
+    
+    -- if vc não tem grana, não compra.
+    if berry < preco  THEN 
+        RAISE EXCEPTION 'Você não tem Berries suficiente para comprar.';
+    end if;
+
+    --                                 preço do item
+    UPDATE jogador SET berries = berries - preco WHERE nome_save = nome_jog and id_personagem = persona;
+
+END;    
+
+$compra2$ LANGUAGE plpgsql;
+
+-- quando 
+
+
+
     -- Quando conclui missão, libera ilha
         -- conclui objetivo, marca ilha liberada
+
+--call compra2(50,'jogador1',1);
+
+
+CREATE or replace FUNCTION  check_missao() RETURNs trigger AS $check_missao$
+DECLARE
+    obj_count INTEGER;
+    obj_comp_count  INTEGER;
+    xp_missao INTEGER;
+    xp_perso INTEGER;
+BEGIN   
+    SELECT COUNT(*) INTO obj_count FROM objetivo_status 
+    WHERE id_missao = OLD.id_missao
+    AND id_jogador_save = OLD.id_jogador_save
+    AND id_jogador_personagem = OLD.id_jogador_personagem;
+
+    SELECT COUNT(*) INTO obj_comp_count FROM objetivo_status
+    WHERE status = 'Concluido'
+    AND id_jogador_save = OLD.id_jogador_save
+    and id_missao = OLD.id_missao
+    AND id_jogador_personagem = OLD.id_jogador_personagem;
+
+    --return obj_comp_count;
+
+    SELECT qtd_experiencia INTO xp_missao FROM missao WHERE id_missao = OLD.id_missao;
+
+    --RAISE EXCEPTION 'Olá mundo % % %',xp_missao,obj_count,obj_comp_count;
+
+
+    IF obj_count = obj_comp_count THEN 
+        UPDATE personagem_principal SET experiencia = experiencia + xp_missao WHERE id_personagem = old.id_jogador_personagem ;
+        --RAISE EXCEPTION 'Olá mundo % % %',xp_missao,1000000000000000,10000000000000;
+        return new;
+    END IF;
+
+
+
+    return new;
+
+
+END;
+
+$check_missao$ LANGUAGE plpgsql;
+
+drop trigger rihana on objetivo_status;
+
+
+CREATE trigger rihana after UPDATE on objetivo_status
+for each ROW EXECUTE PROCEDURE check_missao();
+
+update objetivo_status set status = 'Liberado' where id_missao = 1 and id_objetivo = 1;
+
+select * from objetivo_status ;
+
+
+
+
+update objetivo_status set status = 'Liberado' where id_missao = 1 and id_objetivo = 1;
+select * from objetivo_status ;
+
+update objetivo_status set status = 'Liberado' where id_missao = 1 and id_objetivo = 2;
+select * from objetivo_status ;
+
+update objetivo_status set status = 'Concluido' where id_missao = 1 and id_objetivo = 1;
+select * from objetivo_status ;
+
+update objetivo_status set status = 'Concluido' where id_missao = 1 and id_objetivo = 2;
+
+select * from objetivo_status ;
+
+
+select * from personagem_principal where id_personagem = 1;
+
+
+/*'''
+
+
+CREATE or replace FUNCTION  check_missao() RETURNs trigger AS $check_missao$
+BEGIN
+
+    RAISE EXCEPTION 'Olá mundo % % %',1,1,1;
+    
+END;
+$check_missao$ LANGUAGE plpgsql;
+drop trigger oioioioii
+
+CREATE trigger oioioioii after UPDATE on objetivo
+for each ROW EXECUTE PROCEDURE check_missao();
+'''
+
+*/
+
