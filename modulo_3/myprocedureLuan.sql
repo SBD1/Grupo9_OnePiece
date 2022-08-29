28968c3c920461b5b4d2513e6256ba4c4d07d1d3
Luan-Cavalcante <luanv.cavalcante@gmail.com>
2022-08-29 13:34:56 -0300

    falta tirar do inventário do npc

---
+++ modulo_3/myprocedureLuan.sql
@@ -0,0 +1,41 @@
+CREATE OR REPLACE procedure compra2(id_item_param INTEGER, qtd_item_param INTEGER, nome_jog VARCHAR(30),persona INTEGER)
+AS $compra2$
+declare 
+    berry INTEGER;
+    preco_compra INTEGER;
+    preco_item INTEGER;
+BEGIN
+    Select berries into berry from jogador 
+    WHERE nome_save = nome_jog and id_personagem = persona;
+
+    select preco into preco_item from item where id_item = id_item_param;
+
+    preco_compra = preco_item * qtd_item_param;
+
+    -- if vc não tem grana, não compra.
+    if berry < preco_compra  THEN 
+        RAISE EXCEPTION 'Você não tem Berries suficiente para comprar.';
+    end if;
+
+    -- procura esse item no inventário, se já existir, atualiza
+    perform * from inventario_jogador where id_jogador_save = nome_jog and
+                                            id_jogador_personagem = persona and 
+                                            id_item = id_item_param;
+    IF FOUND THEN
+        UPDATE inventario_jogador set qtd_item = qtd_item + qtd_item_param 
+        where   id_jogador_save = nome_jog and
+                id_jogador_personagem = persona and 
+                id_item = id_item_param;
+    -- se não insere
+    ELSE 
+        INSERT INTO inventario_jogador (id_jogador_save, id_jogador_personagem, id_item, qtd_item) 
+        VALUES (nome_jog, persona, id_item_param, qtd_item_param);
+
+    END IF;
+
+    --                                 preço da compra
+    UPDATE jogador SET berries = berries - preco_compra WHERE nome_save = nome_jog and id_personagem = persona;
+    
+END;    
+
+$compra2$ LANGUAGE plpgsql;
